d008be790a5ce45ff255cbd179ec77f9bd21a866
Thawng Hmung <tthmung@mtu.edu>
2022-02-03 10:58:52 -0500

    database changes

--- DjangoProfile/settings/dev.py
+++ DjangoProfile/settings/dev.py
@@ -4,3 +4,15 @@ from DjangoProfile.settings.base import *
 DEBUG = True
 
 ALLOWED_HOSTS = []
+
+
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
+
