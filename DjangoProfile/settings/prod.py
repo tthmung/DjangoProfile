7294e7928a528d763f404fd3f2650cd350a1092d
Thawng Hmung <tthmung@mtu.edu>
2022-02-04 12:57:14 -0500

    adding more to secret key

--- DjangoProfile/settings/prod.py
+++ DjangoProfile/settings/prod.py
@@ -7,6 +7,17 @@ ALLOWED_HOSTS = ["3.16.7.75", "tthmung.xyz", "www.tthmung.xyz", "localhost"]
 
 # Production only
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': str(os.getenv("POST_PORJ")),
+        'USER': str(os.getenv("POST_USER")),
+        'PASSWORD': str(os.getenv("POST_PASS")),
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
+
 # HTTPS settings
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
