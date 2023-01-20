0f9c1262b443d7f9cfb819053fbbc0f4445bd561
Thawng Hmung <tthmung@mtu.edu>
2023-01-20 13:32:54 -0500

    Add expire header
    Expire header to increase performance

--- experience/views.py
+++ experience/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import *
+from django.views.decorators.cache import cache_page
 
-# Create your views here.
+
+@cache_page(60 * 15)
 def experience(request):
     items = experienceList.objects.all().order_by('-date')
     return render(request, "work.html", {"items": items})
--- myapp/views.py
+++ myapp/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import *
+from django.views.decorators.cache import cache_page
 
-# Create your views here.
+@cache_page(60 * 15)
 def home(request):
     return render(request, "home.html")
 
+@cache_page(60 * 15)
 def project(request):
     post = ProjectList.objects.all().order_by('-date')
     return render(request, "project.html", {"post": post})
